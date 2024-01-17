cf7711394c5b64ac36b5f1dcbc44ebbe7bff9b77
abdumutalovm <mirziyodabdumutalov83@gmail.com>
2024-01-17 23:15:23 +0500

    4-month 3-lesson

---
+++ script.js
@@ -0,0 +1,17 @@
+// ex-1
+let firstNumber = 8;
+let secondNumber = 32;
+let thirdNumber = 14;
+
+let result = 1;
+
+if (firstNumber % 3 == 0) {
+  result *= firstNumber;
+}
+if (secondNumber % 3 == 0) {
+  result *= secondNumber;
+}
+if (thirdNumber % 3 == 0) {
+  result *= thirdNumber;
+}
+console.log(result);
